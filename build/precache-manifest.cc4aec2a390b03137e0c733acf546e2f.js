82806a5ae9eba56254d259d92b1788c33aa67764
dutraneto <dudutraneto@gmail.com>
2019-02-09 12:35:16 -0500

    add build folder

---
+++ build/precache-manifest.cc4aec2a390b03137e0c733acf546e2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "89e2666c24d37055bcb60e9d2d9f7e35",
+    "url": "/react-calculator/build/static/media/Roboto-Thin.89e2666c.ttf"
+  },
+  {
+    "revision": "813a27edb99d9efdcd40",
+    "url": "/react-calculator/build/static/js/runtime~main.813a27ed.js"
+  },
+  {
+    "revision": "529e201d5199efe8b83d",
+    "url": "/react-calculator/build/static/js/main.529e201d.chunk.js"
+  },
+  {
+    "revision": "c7e4fdd9d5a2954f2ea1",
+    "url": "/react-calculator/build/static/js/1.c7e4fdd9.chunk.js"
+  },
+  {
+    "revision": "529e201d5199efe8b83d",
+    "url": "/react-calculator/build/static/css/main.200eeb8c.chunk.css"
+  },
+  {
+    "revision": "26b93d005b496a5b9346aeaac7196cbf",
+    "url": "/react-calculator/build/index.html"
+  }
+];
