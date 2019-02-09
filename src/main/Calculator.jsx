a86ef447eb2da4f948d8982612d3d9d0b019a22c
dutraneto <dudutraneto@gmail.com>
2019-02-08 22:04:59 -0500

    add design and colors

---
+++ src/main/Calculator.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './Calculator.css'
+
+export default class Calculator extends Component {
+    render() {
+        return ( // expression
+
+            <div className="calculator">
+                
+            </div>
+
+        )
+    }
+
+}
